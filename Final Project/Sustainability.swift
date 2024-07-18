f581d95637efb886dc42382737c7b70fc69aaef4
Val <valeryromano12315@gmail.com>
2024-07-18 12:49:30 -0400

    New Changes

---
+++ Final Project/Sustainability.swift
@@ -0,0 +1,18 @@
+//
+//  Sustainability.swift
+//  Final Project
+//
+//  Created by Scholar on 7/18/24.
+//
+
+import SwiftUI
+
+struct Sustainability: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Sustainability()
+}
